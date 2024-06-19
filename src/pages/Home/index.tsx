c5e29d20db4588b7b4f6e1c44b2729d72dcc2968
Stanley-Felix-Bergamo <stanley.felix.bergamo@outlook.com>
2024-06-19 11:58:36 -0300

    commit

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -53,19 +53,24 @@ export function Home() {
     };
     setCycles((state) => [...state, newCycles]);
     setActiveCycleId(id);
+    setAmountSecondsPassed(0);
     reset();
   }
 
   const activeCycle = cycles.find((cycle) => cycle.id === activeCyclesId);
 
   useEffect(() => {
+    let interval: number;
     if (activeCycle) {
-      setInterval(() => {
+      interval = setInterval(() => {
         setAmountSecondsPassed(
           differenceInSeconds(new Date(), activeCycle.startDate)
         );
       }, 1000);
     }
+    return () => {
+      clearInterval(interval);
+    };
   }, [activeCycle]);
 
   const totalSeconds = activeCycle ? activeCycle.minutesAmout * 60 : 0;
@@ -76,6 +81,12 @@ export function Home() {
 
   const minutes = String(minutesAmount).padStart(2, "0");
   const seconds = String(secondsAmount).padStart(2, "0");
+
+  useEffect(() => {
+    if (activeCycle) {
+      document.title = `${minutes}:${seconds}`;
+    }
+  }, [minutes, seconds, activeCycle]);
 
   const task = watch("task");
   const isSubmitDisabled = !task;
